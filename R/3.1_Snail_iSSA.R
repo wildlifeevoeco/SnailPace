e0118815364a548d6dc6941ef3d72aef9584f578
Emily Denief <eed206@mun.ca>
2020-02-24 11:31:13 -0330

    Added P4 model for treatment 3

--- R/3.1_Snail_iSSA.R
+++ R/3.1_Snail_iSSA.R
@@ -284,6 +284,14 @@ P4.treat2.Out<- dat[!(snail %in% badsnails) & Treatment=="2",
                     },
                     by = .(snail)]
 
+P4.treat3.Out<- dat[Treatment=="3",
+                    {
+                      print(.BY[[1]])
+                      P4Model(case_, log_sl, cos_ta, ToD_start, Temperature, log(edgedist_start + 1), 
+                              log(brickdist_start + 1), step_id_)
+                    },
+                    by = .(snail)]
+
 P4ModelOut <- rbind(P4.g1.Out, P4.g2.Out, P4.g3.Out, P4.treats.Out)
 
 #saveRDS(P4ModelOut, '~/snails/Data/derived/P4Model.Rds')
