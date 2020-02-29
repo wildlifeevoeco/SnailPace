11ce098c80ff28bb28c349891fb8e38db2c6e71e
Emily Denief <eed206@mun.ca>
2020-02-29 19:10:07 -0330

    Attempting graphs for treat1, still messy

--- R/4.2_Figures.R
+++ R/4.2_Figures.R
@@ -33,11 +33,13 @@ all.coef <- all.m[term=='coef' & variable!="AIC"]
 
 ba.coef <- all.coef[variable %like% "Stage"]
 
+ba.coef$Treatment <- ifelse(ba.coef$Treatment=="4", "C", ba.coef$Treatment)
+
 # format of data needs to be a column of the covariates (here = term), a column of the values (here = delta), and a column of groupings (in my case COD)
 
 cbPalette = c("red", "blue", "purple", "green")
 
-beta.variables <- ggplot(ba.coef, aes(variable, value, fill = Treatment)) +
+beta.variables <- ggplot(ba.coef, aes(variable, (value) , fill = Treatment)) +
   geom_boxplot(aes(fill = Treatment),# notch = TRUE, notchwidth = 0.7,
                outlier.color = NA, lwd = 0.6,
                alpha = 0.25) +
@@ -63,20 +65,78 @@ beta.variables <- ggplot(ba.coef, aes(variable, value, fill = Treatment)) +
 
 beta.variables
 
-### Extract stage variable ----
-ba.coef$variable2 <- as.character(ba.coef$variable)
+## Extract stage variable 
+ba.coef[,"ba"] <- ifelse(ba.coef$variable %like% "StageA", "A", "B")
 
-#Select position 1 letter after "Stage". Not working...
-stage <- gsub('(.*)([A-Z]-{1})[Stage].*','\\2', ba.coef$variable2) 
-stage
+## New variable of treatment/control and stage combined
+ba.coef[,"stage.ct"] <- paste(ba.coef$Treatment, ba.coef$ba, sep = "")
 
-#This one works but only when variable ends in stage
-stage <- gsub('^(.*)[Stage]','\\2', ba.coef$variable2)
-stage
+saveRDS(ba.coef, '~/snails/Data/derived/ba-coef.Rds')
 
+ba.coef <- readRDS('~/snails/Data/derived/ba-coef.Rds')
 
+### TREATMENT 1 FIGURES ----
 
+treat1coef <-ba.coef[Treatment=="1", value, by = .(stage.ct)]
+treatccoef <-ba.coef[Treatment=="C", value, by = .(stage.ct)]
 
+# TREAT 1
+cbPalette = c("red", "blue")
 
+treat1betas <- ggplot(treat1coef, aes(stage.ct, (value))) +
+  geom_boxplot(aes(fill = stage.ct),# notch = TRUE, notchwidth = 0.7,
+               outlier.color = NA, lwd = 0.6,
+               alpha = 0.25) +
+  geom_jitter(aes(color = stage.ct),
+              position = position_jitterdodge(.35),
+              size = 2, alpha = 0.4) +
+  #ggtitle('Interaction with community identity') +
+  geom_hline(aes(yintercept = 0), lty = 2) +
+  theme(#legend.position = 'none',
+    axis.title = element_text(size = 16, color = 'black'),
+    axis.text = element_text(size = 14, color = 'black'),
+    plot.title=element_text(size = 16, hjust=0),
+    axis.line = element_line(colour = "black"),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    strip.background = element_rect(colour="black", size = 1, fill = "white"),
+    strip.text = element_text(size = 14)) +
+  xlab('') +
+  ylab('beta') +
+  #ggtitle("c) change between control and case") +
+  scale_fill_manual(values = cbPalette) +
+  scale_color_manual(values = cbPalette)# + ylim(-2,2)
 
+cbPalette = c("red", "blue")
+
+#CONTROL
+treatcbetas <- ggplot(treatccoef, aes(stage.ct, (value))) +
+  geom_boxplot(aes(fill = stage.ct),# notch = TRUE, notchwidth = 0.7,
+               outlier.color = NA, lwd = 0.6,
+               alpha = 0.25) +
+  geom_jitter(aes(color = stage.ct),
+              position = position_jitterdodge(.35),
+              size = 2, alpha = 0.4) +
+  #ggtitle('Interaction with community identity') +
+  geom_hline(aes(yintercept = 0), lty = 2) +
+  theme(#legend.position = 'none',
+    axis.title = element_text(size = 16, color = 'black'),
+    axis.text = element_text(size = 14, color = 'black'),
+    plot.title=element_text(size = 16, hjust=0),
+    axis.line = element_line(colour = "black"),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    strip.background = element_rect(colour="black", size = 1, fill = "white"),
+    strip.text = element_text(size = 14)) +
+  xlab('') +
+  ylab('beta') +
+  #ggtitle("c) change between control and case") +
+  scale_fill_manual(values = cbPalette) +
+  scale_color_manual(values = cbPalette)# + ylim(-2,2)
  
+treatcbetas
+
+
+
+
+
