b9b0889ae2cab0945eebada52613b2e3c593ae6e
Emily Denief <eed206@mun.ca>
2020-02-29 14:27:44 -0330

    Working on possible figures

---
+++ R/4.2_Figures.R
@@ -0,0 +1,65 @@
+#### Figures ===
+
+### Packages ----
+libs <- c('ggplot2','data.table', 'dplyr', 'tidyr')
+lapply(libs, require, character.only = TRUE)
+
+### Input data ----
+raw <- '~/snails/Data/raw/'
+derived <- '~/snails/Data/derived/'
+CoreModel <- readRDS('~/snails/Data/derived/CoreModel.Rds')
+P1Model <- readRDS('~/snails/Data/derived/P1Model.Rds')
+P2Model <- readRDS('~/snails/Data/derived/P2Model.Rds')
+P3Model <- readRDS('~/snails/Data/derived/P3Model.Rds')
+P4Model <- readRDS('~/snails/Data/derived/P4Model.Rds')
+
+Core.m <- melt(CoreModel)
+P1.m <- melt(P1Model)
+P2.m <- melt(P2Model)
+P3.m <- melt(P3Model)
+P4.m <- melt(P4Model)
+
+Core.m[,"Treatment"] <- substr(Core.m$snail, 3, 3)
+P1.m[,"Treatment"] <- substr(P1.m$snail, 3, 3)
+P2.m[,"Treatment"] <- substr(P2.m$snail, 3, 3)
+P3.m[,"Treatment"] <- substr(P3.m$snail, 3, 3)
+P4.m[,"Treatment"] <- substr(P4.m$snail, 3, 3)
+
+all.m <- rbind(Core.m, P1.m, P2.m, P3.m, P4.m)
+
+all.coef <- all.m[term=='coef' & variable!="AIC"]
+
+ba.coef <- all.coef[variable %like% "Stage"]
+
+# format of data needs to be a column of the covariates (here = term), a column of the values (here = delta), and a column of groupings (in my case COD)
+
+cbPalette = c("red", "blue", "purple", "green")
+
+beta.variables <- ggplot(ba.coef, aes(variable, value, fill = Treatment)) +
+  geom_boxplot(aes(fill = Treatment),# notch = TRUE, notchwidth = 0.7,
+               outlier.color = NA, lwd = 0.6,
+               alpha = 0.25) +
+  geom_jitter(aes(color = Treatment),
+              position = position_jitterdodge(.35),
+              size = 2, alpha = 0.4) +
+  #ggtitle('Interaction with community identity') +
+  geom_hline(aes(yintercept = 0), lty = 2) +
+  theme(#legend.position = 'none',
+    axis.title = element_text(size = 16, color = 'black'),
+    axis.text = element_text(size = 14, color = 'black'),
+    plot.title=element_text(size = 16, hjust=0),
+    axis.line = element_line(colour = "black"),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    strip.background = element_rect(colour="black", size = 1, fill = "white"),
+    strip.text = element_text(size = 14)) +
+  xlab('') +
+  ylab('beta') +
+  #ggtitle("c) change between control and case") +
+  scale_fill_manual(values = cbPalette) +
+  scale_color_manual(values = cbPalette)# + ylim(-2,2)
+
+beta.variables
+
+unique(all.coef$variable)
+ 
