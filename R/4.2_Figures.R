e8bd83d2664d06ed559e5829dfa3e702be85d15a
Emily Denief <eed206@mun.ca>
2020-03-09 08:58:15 -0230

    Moving over to "Figures_TIDIED" because this is messy

--- R/4.2_Figures.R
+++ R/4.2_Figures.R
@@ -1,7 +1,7 @@
 #### Figures ====
 
 ### Packages ----
-libs <- c('ggplot2','ggpubr','data.table', 'dplyr', 'tidyr', 'tidyverse')
+libs <- c('ggplot2','ggpubr','data.table', 'dplyr', 'tidyr', 'tidyverse', 'patchwork')
 lapply(libs, require, character.only = TRUE)
 
 ### Input data ----
@@ -14,6 +14,9 @@ P3Model <- readRDS('~/snails/Data/derived/P3Model.Rds')
 P4Model <- readRDS('~/snails/Data/derived/P4Model.Rds')
 
 ### Combine models ----
+#nbricks has to be character
+P2Model[,"nbricks"] <- as.character(P2Model$nbricks)
+P4Model[,"nbricks"] <- as.character(P4Model$nbricks)
 
 Core.m <- melt(CoreModel)
 P1.m <- melt(P1Model)
@@ -35,35 +38,52 @@ ba.coef <- all.coef[variable %like% "Stage" & !(variable %like% "StageAcc") ]
 
 ba.coef$Treatment <- ifelse(ba.coef$Treatment=="4", "C", ba.coef$Treatment)
 
-# format of data needs to be a column of the covariates (here = term), a column of the values (here = delta), and a column of groupings (in my case COD)
-### ALL TREATS ====
-cbPalette = c("red", "blue", "purple", "green")
+#### P1 ----
+p1.coef <- P1.m[variable %like% "Stage" & !(variable %like% "StageAcc") ]
 
-beta.variables <- ggplot(ba.coef, aes(variable, (value) , fill = Treatment)) +
-  geom_boxplot(aes(fill = Treatment),# notch = TRUE, notchwidth = 0.7,
-               outlier.color = NA, lwd = 0.6,
-               alpha = 0.25) +
-  geom_jitter(aes(color = Treatment),
-              position = position_jitterdodge(.35),
-              size = 2, alpha = 0.4) +
-  #ggtitle('Interaction with community identity') +
-  geom_hline(aes(yintercept = 0), lty = 2) +
-  theme(#legend.position = 'none',
-    axis.title = element_text(size = 16, color = 'black'),
-    axis.text = element_text(size = 14, color = 'black'),
-    plot.title=element_text(size = 16, hjust=0),
-    axis.line = element_line(colour = "black"),
-    panel.grid.minor = element_blank(),
-    panel.background = element_blank(),
-    strip.background = element_rect(colour="black", size = 1, fill = "white"),
-    strip.text = element_text(size = 14)) +
-  xlab('') +
-  ylab('beta') +
-  #ggtitle("c) change between control and case") +
-  scale_fill_manual(values = cbPalette) +
-  scale_color_manual(values = cbPalette)# + ylim(-2,2)
+p1.coef[,"ba"] <- ifelse(p1.coef$variable %like% "StageA", "A", "B")
+p1.coef[,"ba"] <- factor(p1.coef$ba, levels = c("B", "A"))
+p1.coef$Treatment <- ifelse(p1.coef$Treatment=="4", "C", p1.coef$Treatment)
+## New variable of treatment/control and stage combined
 
-beta.variables
+p1.coef[,"stage.ct"] <- paste(p1.coef$Disturbance, p1.coef$ba, sep = "")
+
+
+p1.coef[,"variable2"] <- str_remove_all((p1.coef$variable), "StageA")
+p1.coef[,"variable3"] <- str_remove_all((p1.coef$variable2), "StageB")
+p1.coef[,"variable4"] <- str_remove_all((p1.coef$variable3), ":")
+
+p1.coef <- p1.coef[,.(snail, term, value, Disturbance, ba, variable=variable4, nbricks)]
+
+# # format of data needs to be a column of the covariates (here = term), a column of the values (here = delta), and a column of groupings (in my case COD)
+# ### ALL TREATS ====
+# cbPalette = c("red", "blue", "purple", "green")
+# 
+# beta.variables <- ggplot(ba.coef, aes(variable, (value) , fill = Treatment)) +
+#   geom_boxplot(aes(fill = Treatment),# notch = TRUE, notchwidth = 0.7,
+#                outlier.color = NA, lwd = 0.6,
+#                alpha = 0.25) +
+#   geom_jitter(aes(color = Treatment),
+#               position = position_jitterdodge(.35),
+#               size = 2, alpha = 0.4) +
+#   #ggtitle('Interaction with community identity') +
+#   geom_hline(aes(yintercept = 0), lty = 2) +
+#   theme(#legend.position = 'none',
+#     axis.title = element_text(size = 16, color = 'black'),
+#     axis.text = element_text(size = 14, color = 'black'),
+#     plot.title=element_text(size = 16, hjust=0),
+#     axis.line = element_line(colour = "black"),
+#     panel.grid.minor = element_blank(),
+#     panel.background = element_blank(),
+#     strip.background = element_rect(colour="black", size = 1, fill = "white"),
+#     strip.text = element_text(size = 14)) +
+#   xlab('') +
+#   ylab('beta') +
+#   #ggtitle("c) change between control and case") +
+#   scale_fill_manual(values = cbPalette) +
+#   scale_color_manual(values = cbPalette)# + ylim(-2,2)
+# 
+# beta.variables
 
 ## Extract stage variable 
 ba.coef[,"ba"] <- ifelse(ba.coef$variable %like% "StageA", "A", "B")
@@ -72,7 +92,7 @@ ba.coef[,"ba"] <- ifelse(ba.coef$variable %like% "StageA", "A", "B")
 ba.coef[,"stage.ct"] <- paste(ba.coef$Treatment, ba.coef$ba, sep = "")
 
 
-stopwords <- c(":StageA:", ":StageB:", "StageA:", ":StageA", "StageB:", ":StageB")
+## Extract variables!!
 
 ba.coef[,"variable2"] <- str_remove_all((ba.coef$variable), "StageA")
 ba.coef[,"variable3"] <- str_remove_all((ba.coef$variable2), "StageB")
@@ -81,43 +101,36 @@ ba.coef[,"variable4"] <- str_remove_all((ba.coef$variable3), ":")
 ba.coef <- ba.coef[,.(snail, term, value, Treatment, ba, stage.ct, variable=variable4)]
 
 ## Rename variables
-
-ba.coef.names <- unique(ba.coef$variable)
-
-ba.coef.betas.names <- c("log_sl", "cos_ta",
-                              "land_end_adjforest", "land_end_adjopen", "land_end_adjwet", "log(1 + roadDist_end)",
-                              "log(1 + distance2)", "log(1 + packDistadj_end)",
-                              "log(ttd1 + 1):log_sl", "log(ttd1 + 1):cos_ta",
-                              "log(ttd1 + 1):land_end_adjforest", "log(ttd1 + 1):land_end_adjopen", "log(ttd1 + 1):land_end_adjwet", "log(ttd1 + 1):log(1 + roadDist_end)",
-                              "log(ttd1 + 1):log(1 + distance2)", "log(ttd1 + 1):log(1 + packDistadj_end)")
-
-everyone.all.indiv.betas <- everyone.all.indiv[term %chin% everyone.all.betas.names]
-# everyone.betas <- c("log_sl", "cos_ta", "land_end_adjforest", "land_end_adjopen", "land_end_adjwet", "log(1 + roadDist_end)",
-#                 "log(1 + distance2)", "log(1 + packDistadj_end)")
-
-everyone.all.indiv.betas$term <- factor(everyone.all.indiv.betas$term, levels = everyone.all.betas.names, labels = c("log_sl", "cos_ta",'forest', "open", "wet", "roadDist",
-                                                                                                                     "nnDist", "boundaryDist",
-                                                                                                                     "log_sl-ttd", "cos_ta-ttd", "forest-ttd", "open-ttd", "wet-ttd", "roadDist-ttd",
-                                                                                                                     "nnDist-ttd", "boundaryDist-ttd"))
+# 
+# ba.coef.names <- unique(ba.coef$variable)
+# 
+# ba.coef.betas.names <- c("edgedist_start", "edgedist_end", "brickdist_start", "brickdist_end", "SL", "TA", 
+#                          "SLbrickdist_start","SLbrickdist_end", "TAbrickdist_start", "TAbrickdist_end",
+#                          "SLedgedist_start", "SLedgedist_end", "TAedgedist_start", "TAedgedist_end",)
+# 
+# all.indiv.betas <- everyone.all.indiv[term %chin% everyone.all.betas.names]
+# # everyone.betas <- c("log_sl", "cos_ta", "land_end_adjforest", "land_end_adjopen", "land_end_adjwet", "log(1 + roadDist_end)",
+# #                 "log(1 + distance2)", "log(1 + packDistadj_end)")
+# 
+# everyone.all.indiv.betas$term <- factor(everyone.all.indiv.betas$term, levels = everyone.all.betas.names, labels = c("log_sl", "cos_ta",'forest', "open", "wet", "roadDist",
+#                                                                                                                      "nnDist", "boundaryDist",
+#                                                                                                                      "log_sl-ttd", "cos_ta-ttd", "forest-ttd", "open-ttd", "wet-ttd", "roadDist-ttd",
+#                                                                                                                      "nnDist-ttd", "boundaryDist-ttd"))
 
 saveRDS(ba.coef, '~/snails/Data/derived/ba-coef.Rds')
 
 ### Read in ba.coef ----
 ba.coef <- readRDS('~/snails/Data/derived/ba-coef.Rds')
 
-### TREATMENT 1 FIGURES ----
+### P1 FIGURES ----
 
-treat1coef <-ba.coef[Treatment=="1", value, by = .(stage.ct)]
-treatccoef <-ba.coef[Treatment=="C", value, by = .(stage.ct)]
 
-# TREAT 1
 cbPalette = c("red", "blue")
-
-treat1betas <- ggplot(treat1coef, aes(stage.ct, (value))) +
-  geom_boxplot(aes(fill = stage.ct),# notch = TRUE, notchwidth = 0.7,
+p1.control1 <- ggplot(p1.coef[Disturbance=="Control" & nbricks=="1"], aes(variable, (value))) +
+  geom_boxplot(aes(fill = ba),# notch = TRUE, notchwidth = 0.7,
                outlier.color = NA, lwd = 0.6,
                alpha = 0.25) +
-  geom_jitter(aes(color = stage.ct),
+  geom_jitter(aes(color = ba),
               position = position_jitterdodge(.35),
               size = 2, alpha = 0.4) +
   #ggtitle('Interaction with community identity') +
@@ -133,21 +146,21 @@ treat1betas <- ggplot(treat1coef, aes(stage.ct, (value))) +
     strip.text = element_text(size = 14)) +
   xlab('') +
   ylab('beta') +
-  #ggtitle("c) change between control and case") +
-  scale_fill_manual(values = cbPalette) +
-  scale_color_manual(values = cbPalette) + 
-  ylim(-15,15)
+  ggtitle("a) control") +
+  # scale_fill_manual(values = cbPalette) +
+  # scale_color_manual(values = cbPalette) + 
+  ylim(-10,35)
 
-treat1betas
+p1.control1
 
-#CONTROL
+#Disturbances
 
 cbPalette = c("red", "blue")
-treatcbetas <- ggplot(treatccoef, aes(stage.ct, (value))) +
-  geom_boxplot(aes(fill = stage.ct),# notch = TRUE, notchwidth = 0.7,
+p1.brick1 <- ggplot(p1.coef[Disturbance=="Disturbed" & nbricks=="1"], aes(variable, (value))) +
+  geom_boxplot(aes(fill = ba),# notch = TRUE, notchwidth = 0.7,
                outlier.color = NA, lwd = 0.6,
                alpha = 0.25) +
-  geom_jitter(aes(color = stage.ct),
+  geom_jitter(aes(color = ba),
               position = position_jitterdodge(.35),
               size = 2, alpha = 0.4) +
   #ggtitle('Interaction with community identity') +
@@ -163,12 +176,139 @@ treatcbetas <- ggplot(treatccoef, aes(stage.ct, (value))) +
     strip.text = element_text(size = 14)) +
   xlab('') +
   ylab('beta') +
-  #ggtitle("c) change between control and case") +
-  scale_fill_manual(values = cbPalette) +
-  scale_color_manual(values = cbPalette) + 
+  ggtitle("b) disturbed") +
+  # scale_fill_manual(values = cbPalette) +
+  # scale_color_manual(values = cbPalette) + 
+  ylim(-10,35)
+
+p1.brick1
+
+p1.control1/p1.brick1
+
+# Treatment 2
+
+cbPalette = c("red", "blue")
+p1.control2 <- ggplot(p1.coef[Disturbance=="Control" & nbricks=="2"], aes(variable, (value))) +
+  geom_boxplot(aes(fill = ba),# notch = TRUE, notchwidth = 0.7,
+               outlier.color = NA, lwd = 0.6,
+               alpha = 0.25) +
+  geom_jitter(aes(color = ba),
+              position = position_jitterdodge(.35),
+              size = 2, alpha = 0.4) +
+  #ggtitle('Interaction with community identity') +
+  geom_hline(aes(yintercept = 0), lty = 2) +
+  theme(#legend.position = 'none',
+    axis.title = element_text(size = 16, color = 'black'),
+    axis.text = element_text(size = 14, color = 'black'),
+    plot.title=element_text(size = 16, hjust=0),
+    axis.line = element_line(colour = "black"),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    strip.background = element_rect(colour="black", size = 1, fill = "white"),
+    strip.text = element_text(size = 14)) +
+  xlab('') +
+  ylab('beta') +
+  ggtitle("a) control") +
+  # scale_fill_manual(values = cbPalette) +
+  # scale_color_manual(values = cbPalette) + 
   ylim(-15,15)
- 
-treatcbetas
+
+p1.control2
+
+#Disturbances
+
+cbPalette = c("red", "blue")
+p1.brick2 <- ggplot(p1.coef[Disturbance=="Disturbed" & nbricks=="2"], aes(variable, (value))) +
+  geom_boxplot(aes(fill = ba),# notch = TRUE, notchwidth = 0.7,
+               outlier.color = NA, lwd = 0.6,
+               alpha = 0.25) +
+  geom_jitter(aes(color = ba),
+              position = position_jitterdodge(.35),
+              size = 2, alpha = 0.4) +
+  #ggtitle('Interaction with community identity') +
+  geom_hline(aes(yintercept = 0), lty = 2) +
+  theme(#legend.position = 'none',
+    axis.title = element_text(size = 16, color = 'black'),
+    axis.text = element_text(size = 14, color = 'black'),
+    plot.title=element_text(size = 16, hjust=0),
+    axis.line = element_line(colour = "black"),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    strip.background = element_rect(colour="black", size = 1, fill = "white"),
+    strip.text = element_text(size = 14)) +
+  xlab('') +
+  ylab('beta') +
+  ggtitle("b) disturbed") +
+  # scale_fill_manual(values = cbPalette) +
+  # scale_color_manual(values = cbPalette) + 
+  ylim(-15,15)
+
+p1.brick2
+
+p1.control2/p1.brick2
+
+# Treatment 3
+
+cbPalette = c("red", "blue")
+p1.control3 <- ggplot(p1.coef[Disturbance=="Control" & nbricks=="4"], aes(variable, (value))) +
+  geom_boxplot(aes(fill = ba),# notch = TRUE, notchwidth = 0.7,
+               outlier.color = NA, lwd = 0.6,
+               alpha = 0.25) +
+  geom_jitter(aes(color = ba),
+              position = position_jitterdodge(.35),
+              size = 2, alpha = 0.4) +
+  #ggtitle('Interaction with community identity') +
+  geom_hline(aes(yintercept = 0), lty = 2) +
+  theme(#legend.position = 'none',
+    axis.title = element_text(size = 16, color = 'black'),
+    axis.text = element_text(size = 14, color = 'black'),
+    plot.title=element_text(size = 16, hjust=0),
+    axis.line = element_line(colour = "black"),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    strip.background = element_rect(colour="black", size = 1, fill = "white"),
+    strip.text = element_text(size = 14)) +
+  xlab('') +
+  ylab('beta') +
+  ggtitle("a) control") +
+  # scale_fill_manual(values = cbPalette) +
+  # scale_color_manual(values = cbPalette) + 
+  ylim(-15,15)
+
+p1.control3
+
+#Disturbances
+
+cbPalette = c("red", "blue")
+p1.brick3 <- ggplot(p1.coef[Disturbance=="Disturbed" & nbricks=="4"], aes(variable, (value))) +
+  geom_boxplot(aes(fill = ba),# notch = TRUE, notchwidth = 0.7,
+               outlier.color = NA, lwd = 0.6,
+               alpha = 0.25) +
+  geom_jitter(aes(color = ba),
+              position = position_jitterdodge(.35),
+              size = 2, alpha = 0.4) +
+  #ggtitle('Interaction with community identity') +
+  geom_hline(aes(yintercept = 0), lty = 2) +
+  theme(#legend.position = 'none',
+    axis.title = element_text(size = 16, color = 'black'),
+    axis.text = element_text(size = 14, color = 'black'),
+    plot.title=element_text(size = 16, hjust=0),
+    axis.line = element_line(colour = "black"),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    strip.background = element_rect(colour="black", size = 1, fill = "white"),
+    strip.text = element_text(size = 14)) +
+  xlab('') +
+  ylab('beta') +
+  ggtitle("b) disturbed") +
+  # scale_fill_manual(values = cbPalette) +
+  # scale_color_manual(values = cbPalette) + 
+  ylim(-15,15)
+
+p1.brick2
+
+p1.control2/p1.brick2
+
 
 ##Combined treat 1 and control
 figuretreat1 <- ggarrange(treat1betas, treatcbetas,
