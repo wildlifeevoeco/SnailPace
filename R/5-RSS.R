d14a9c083489c4216fcabc49409d531eabe2b214
Julie Turner <julwturner@gmail.com>
2020-06-08 13:00:34 -0230

    indiv fit distr graphs

--- R/5-RSS.R
+++ R/5-RSS.R
@@ -886,6 +886,125 @@ ggplot(gam.snails) +
   geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
   facet_wrap(vars(snail))
 
+ggplot(gam.snails[snail==snails[1]]) +
+  geom_histogram(aes(sl_, y = ..density..), bins=15) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle(gam.snails$snail)
+
+o11a <-ggplot(gam.snails[snail=='O11a']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=15) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('O11a')
+
+o11b <-ggplot(gam.snails[snail=='O11b']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=15) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('O11b')
+
+o12b <-ggplot(gam.snails[snail=='O12b']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=15) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('O12b')
+
+o13a <-ggplot(gam.snails[snail=='O13a']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=15) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('O13a')
+
+o14a <-ggplot(gam.snails[snail=='O14a']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=15) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('O14a')
+
+o22a <-ggplot(gam.snails[snail=='O22a']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=20) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('O22a')
+
+o22b <-ggplot(gam.snails[snail=='O22b']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=15) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('O22b')
+
+o23a <-ggplot(gam.snails[snail=='O23a']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=15) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('O23a')
+
+o24a <-ggplot(gam.snails[snail=='O24a']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=15) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('O24a')
+
+o24b <-ggplot(gam.snails[snail=='O24b']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=15) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('O24b')
+
+o31a <-ggplot(gam.snails[snail=='O31a']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=10) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('O31a')
+
+p12a <-ggplot(gam.snails[snail=='P12a']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=15) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('P12a')
+
+p13a <-ggplot(gam.snails[snail=='P13a']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=15) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('P13a')
+
+p14a <-ggplot(gam.snails[snail=='P14a']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=15) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('P14a')
+
+p21a <-ggplot(gam.snails[snail=='P21a']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=15) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('P21a')
+
+p22a <-ggplot(gam.snails[snail=='P22a']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=15) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('P22a')
+
+p22b <-ggplot(gam.snails[snail=='P22b']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=15) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('P22b')
+
+p23a <-ggplot(gam.snails[snail=='P23a']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=15) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('P23a')
+
+p23b <-ggplot(gam.snails[snail=='P23b']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=15) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('P23b')
+
+p24a <-ggplot(gam.snails[snail=='P24a']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=15) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('P24a')
+
+p24b <-ggplot(gam.snails[snail=='P24b']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=10) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('P24b')
+
+p31a <-ggplot(gam.snails[snail=='P31a']) +
+  geom_histogram(aes(sl_, y = ..density..), bins=15) +
+  geom_line(aes(x=sl_, y=dgamma(sl_, shape[1], rate[1])), color="blue", size = 1) +
+  ggtitle('P31a')
+
+
+(o11a|o11b|o12b|o13a|o14a)/(o22a|o22b|o23a|o24a|o24b)/(o31a|p12a|p13a|p14a|p21a)/
+  (p22a|p22b|p23a|p23b|p24a)/(p24b|p31a)
+
 #### ALL MODELS ####
 
 #all.mods <- rbind(core.mods, p1.mods, p3.mods)
