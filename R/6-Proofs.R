c66dc839dcacf879d47d12cd1ee76b1cbefb2dfe
Julie Turner <julwturner@gmail.com>
2020-07-02 13:58:01 -0230

    updating graphs

--- R/6-Proofs.R
+++ R/6-Proofs.R
@@ -37,9 +37,10 @@ hr.proof[iteration == 0, observed := 'yes']
 hr.proof[iteration != 0, observed := 'no']
 hr.proof[,'snailsIter'] <- paste(hr.proof$snails2, hr.proof$iteration, sep = '.')
 
-p1 <- ggplot(data=hr.proof [snail != 'O11b' & ed.spd.before>=0], aes(x=edist, y=ed.spd.before, color = randSamp)) + 
-  geom_line(aes(group = snailsIter, linetype = observed), size=1, alpha=.5) +
-  geom_smooth(aes(linetype = observed, fill= randSamp), size = 2, se = T, method = 'lm')+
+
+p1 <- ggplot(data=hr.proof [bd.spd.before>=0], aes(x=bdist, y=bd.spd.before)) + 
+  #geom_smooth(data=hr.proof [bd.spd.before>=0 & observed == 'yes'], aes(color = randSamp), size=2, method = 'lm') +
+  geom_smooth(data=hr.proof [bd.spd.before>=0 & observed == 'no'], aes(color = randSamp), size = 1, method = 'lm')+
   theme_classic() +
   theme(text = element_text(size=15)) +
   theme(plot.title = element_text(hjust = 0.5)) +
@@ -48,7 +49,7 @@ p1 <- ggplot(data=hr.proof [snail != 'O11b' & ed.spd.before>=0], aes(x=edist, y=
   #ylim(-5, 75) +
   theme(plot.margin = margin(0.1, 1, .1, .1, "cm")) +
   ggtitle("a) before ") +
-  xlab("Distance from edge (cm)") + ylab("Speed (m per hour)")
+  xlab("Distance from brick (cm)") + ylab("Speed (m per hour)")
 p1
 
 
@@ -69,11 +70,13 @@ dist.proof[iteration != 0, observed := 'no']
 dist.proof[,'snailsIter'] <- paste(dist.proof$snails2, dist.proof$iteration, sep = '.')
 
 p2 <- ggplot(data=dist.proof[var == 'edgedist'& BA=='before' & brick != 'g1' & brick != 'g3'], 
-             aes(x, rss, colour= randSamp)) +
+             aes(x, rss)) +
+ # geom_smooth(data=dist.proof[var == 'edgedist'& BA=='before' & brick != 'g1' & brick != 'g3' & observed == 'yes'], aes(color = randSamp), size=2) +
+  geom_smooth(data=dist.proof[var == 'edgedist'& BA=='before' & brick != 'g1' & brick != 'g3' & observed == 'no'], aes(color = randSamp), size = 1, method = 'lm')+
   #geom_line(aes(group = snail,alpha = .0001), linetype ='twodash', show.legend = F) +
   #geom_line(data=p1.rss[var == 'edgedist'& BA=='before'],aes(step,disturbance.rss, group = disturbance), size = 1) +
   #geom_point(shape = 1, aes(alpha = .001), show.legend = F) +
-  geom_smooth(size = 1.5, aes(fill = randSamp, linetype = observed), se = T, method = 'lm') +
+  #geom_smooth(size = 1.5, aes(fill = randSamp, linetype = observed), se = T, method = 'lm') +
   # geom_line(data=logRSS.pop[var == 'forest'& ttd=='1 day'], aes(x, rss, colour=COD)) +
   geom_hline(yintercept = 0,colour = "black",lty = 2, size = .7) +
   #geom_ribbon(aes(x, ymin = (rss - 1.96*se), ymax = (rss + 1.96*se), fill=COD, alpha = .2))+
@@ -96,26 +99,27 @@ p2
 #### proof treatment diffs ####
 
 rss.treat <- lapply(seq(0, iterations), function(iter){
-  sub<-unique(p1.rss.all[,.(snail, disturbance)])
+  sub<-unique(p1.rss.all[,.(snail, brick2)])
   if (iter == 0) {
-    sub[, randSamp := disturbance]
+    sub[, randSamp := brick2]
   } else {
-    sub[, randSamp := sample(disturbance)]
+    sub[, randSamp := sample(brick2)]
   }
   sub[,iteration:=iter]
 })
 rss.treat.ls <- rbindlist(rss.treat)
-treat.proof <- merge(p1.rss.all, rss.treat.ls, by= c('snail', 'disturbance'), allow.cartesian = T)
+treat.proof <- merge(p1.rss.all, rss.treat.ls, by= c('snail', 'brick2'), allow.cartesian = T)
 treat.proof[iteration == 0, observed := 'yes']
 treat.proof[iteration != 0, observed := 'no']
 treat.proof[,'snailsIter'] <- paste(treat.proof$snails2, treat.proof$iteration, sep = '.')
 
-p3 <- ggplot(data=dist.proof[var == 'edgedist'& BA=='before' & brick != 'g1' & brick != 'g3'], 
+p3 <- ggplot(data=treat.proof[var == 'edgedist'& BA=='after' & brick != 'g1' & brick != 'g3'], 
              aes(x, rss, colour= randSamp)) +
+  geom_smooth(data=treat.proof[var == 'edgedist'& BA=='after' & brick != 'g1' & brick != 'g3' & observed == 'no'], aes(color = randSamp), size = 1, method = 'lm')+
   #geom_line(aes(group = snail,alpha = .0001), linetype ='twodash', show.legend = F) +
   #geom_line(data=p1.rss[var == 'edgedist'& BA=='before'],aes(step,disturbance.rss, group = disturbance), size = 1) +
   #geom_point(shape = 1, aes(alpha = .001), show.legend = F) +
-  geom_smooth(size = 1.5, aes(fill = randSamp, linetype = observed), se = T, method = 'lm') +
+  #geom_smooth(size = 1.5, aes(fill = randSamp, linetype = observed), se = T, method = 'lm') +
   # geom_line(data=logRSS.pop[var == 'forest'& ttd=='1 day'], aes(x, rss, colour=COD)) +
   geom_hline(yintercept = 0,colour = "black",lty = 2, size = .7) +
   #geom_ribbon(aes(x, ymin = (rss - 1.96*se), ymax = (rss + 1.96*se), fill=COD, alpha = .2))+
@@ -129,7 +133,7 @@ p3 <- ggplot(data=dist.proof[var == 'edgedist'& BA=='before' & brick != 'g1' & b
   theme(axis.text.x = element_text(margin=margin(10,10,10,10,"pt")),
         axis.text.y = element_text(margin=margin(10,10,10,10,"pt")))+ theme(axis.ticks.length = unit(-0.25, "cm")) +
   ylab("logRSS") + xlab("Distance from edge (cm)") +
-  ggtitle("before")  +
+  ggtitle("after")  +
   #ylim(-0.7,1.3) +
   # scale_colour_manual("", values = c("gray", "black", "gray33", 'blue'))  +  
   theme(legend.key = element_blank()) + theme(legend.position = 'right') + theme(legend.text = element_text(size = 10))
