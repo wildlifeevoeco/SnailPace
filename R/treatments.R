a74ddb6bd7950498309d9522997d080464f6b760
Alec L. Robitaille <robit.alec@gmail.com>
2021-08-12 17:00:57 -0700

    fix missing sub treatment and drop rep columns

--- R/treatments.R
+++ R/treatments.R
@@ -3,13 +3,17 @@
 
 brick_treatments <- function(DT, treatments) {
   new_names <- c('brickdist_start', 'brickdist_end')
-  
+  drop_names <- c(paste0('brickedge', c(1, 2, 3), rep(c('_start', '_end'), each = 3)))
   l <- lapply(treatments, function(treat) {
+    zz <- copy(DT)[treatment == treat]
+    zz[, ghostbricks := treat]
     setnames(
-      copy(DT), 
+      zz, 
       c(paste0('brickedge', treat, '_start'), paste0('brickedge', treat, '_end')), 
       new_names
-    )[, ghostbricks := treat]
+    )
+    zz[, (drop_names) := NULL]
+    zz
   })
   
   rbindlist(l)
