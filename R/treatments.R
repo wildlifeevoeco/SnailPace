d14b900e54448f731dc0d9e377ac3f3ff5e7d908
Alec L. Robitaille <robit.alec@gmail.com>
2021-08-12 17:06:36 -0700

    drop treatments arg

--- R/treatments.R
+++ R/treatments.R
@@ -1,9 +1,10 @@
 
 # === Treatments ----------------------------------------------------------
 
-brick_treatments <- function(DT, treatments) {
+brick_treatments <- function(DT) {
+  treatments <- c(1, 2, 3)
   new_names <- c('brickdist_start', 'brickdist_end')
-  drop_names <- c(paste0('brickedge', c(1, 2, 3), rep(c('_start', '_end'), each = 3)))
+  drop_names <- c(paste0('brickedge', treatments, rep(c('_start', '_end'), each = 3)))
   l <- lapply(treatments, function(treat) {
     zz <- copy(DT)[treatment == treat]
     zz[, ghostbricks := treat]
