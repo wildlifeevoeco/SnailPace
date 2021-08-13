3af47eeeb5cec4f92bcea5b62b0acc248ba33458
Alec L. Robitaille <robit.alec@gmail.com>
2021-08-13 10:40:24 -0700

    reflow

--- R/models.R
+++ R/models.R
@@ -2,22 +2,23 @@
 
 
 model_p1 <- function(DT) {
-  p1 <- glmmTMB(case_ ~ 
-                  I(log(sl_+1)):temp +
-                  #I(log(sl_+1)):stage:ghostbricks + 
-                  I(log(brickdist_end + 1)):stage:ghostbricks +
-                  I(log(edgedist_end + 1)):stage:ghostbricks +
-                  ## random effects  
-                  (1|indiv_treat_step_id) + 
-                  (0 + I(log(sl_+1))|id_treat) +
-                  (0 + I(log(sl_+1)):stage|id_treat) +
-                  (0 + I(log(brickdist_end + 1)):stage|id_treat) +
-                  (0 + I(log(edgedist_end + 1)):stage|id_treat),
-                family=poisson(), 
-                data = DT,  
-                map = list(theta=factor(c(NA,1:10))), 
-                start = list(theta=c(log(1000), seq(0,0, length.out = 10))))
-
+  p1 <- glmmTMB(
+    case_ ~
+      I(log(sl_ + 1)):temp +
+      #I(log(sl_+1)):stage:ghostbricks +
+      I(log(brickdist_end + 1)):stage:ghostbricks +
+      I(log(edgedist_end + 1)):stage:ghostbricks +
+      ## random effects
+      (1 | indiv_treat_step_id) +
+      (0 + I(log(sl_ + 1)) | id_treat) +
+      (0 + I(log(sl_ + 1)):stage | id_treat) +
+      (0 + I(log(brickdist_end + 1)):stage | id_treat) +
+      (0 + I(log(edgedist_end + 1)):stage | id_treat),
+    family = poisson(),
+    data = DT,
+    map = list(theta = factor(c(NA, 1:10))),
+    start = list(theta = c(log(1000), seq(0, 0, length.out = 10)))
+  )
 }
 
 
@@ -31,21 +32,26 @@ tidy_model <- function(model, effect) {
 
 
 model_p3 <- function(DT) {
-  p3 <- glmmTMB(case_ ~ 
-                  I(log(sl_+1)):temp +
-                  I(log(sl_+1)):stage:ghostbricks + 
-                  I(log(brickdist_start + 1)):I(log(sl_+1)):stage:ghostbricks +
-                  I(log(edgedist_start + 1)):I(log(sl_+1)):stage:ghostbricks +
-                  ## random effects  
-                  (1|indiv_treat_step_id) + 
-                  (0 + I(log(sl_+1))|id_treat) +
-                  (0 + I(log(sl_+1)):stage|id_treat) +
-                  (0 + I(log(brickdist_start + 1)):I(log(sl_+1)):stage|id_treat) +
-                  (0 + I(log(edgedist_start + 1)):I(log(sl_+1)):stage|id_treat),
-                family=poisson(), 
-                data = DT,  
-                map = list(theta=factor(c(NA,1:10))), 
-                start = list(theta=c(log(1000), seq(0,0, length.out = 10))))
+  p3 <- glmmTMB(
+    case_ ~
+      I(log(sl_ + 1)):temp +
+      I(log(sl_ + 1)):stage:ghostbricks +
+      I(log(brickdist_start + 1)):I(log(sl_ + 1)):stage:ghostbricks +
+      I(log(edgedist_start + 1)):I(log(sl_ + 1)):stage:ghostbricks +
+      ## random effects
+      (1 | indiv_treat_step_id) +
+      (0 + I(log(sl_ + 1)) | id_treat) +
+      (0 + I(log(sl_ + 1)):stage | id_treat) +
+      (0 + I(log(
+        brickdist_start + 1
+      )):I(log(sl_ + 1)):stage | id_treat) +
+      (0 + I(log(edgedist_start + 1)):I(log(sl_ + 1)):stage |
+         id_treat),
+    family = poisson(),
+    data = DT,
+    map = list(theta = factor(c(NA, 1:10))),
+    start = list(theta = c(log(1000), seq(0, 0, length.out = 10)))
+  )
 }
 
 
