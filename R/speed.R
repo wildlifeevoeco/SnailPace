dd4893fb3b676707659013f6c4e349dbd53648c3
Alec L. Robitaille <robit.alec@gmail.com>
2021-08-25 18:44:59 -0700

    plot_speed_edge and plot_speed_brick functions

--- R/speed.R
+++ R/speed.R
@@ -62,86 +62,91 @@ predict_speed <- function(coefs) {
         by = .(id, brick)]
   coefs[, edist := seq(0, maxedge, length.out = 100), 
         by = .(id, brick)]
-  coefs
+  
+  coefs[, disturbance := ifelse(brick %like% 'g', 'undisturbed', 'disturbed'), 
+        by = .(id, brick)]
   
 }
 
 
 
 
+# TODO: necessary?
+# speed <- coefs[,.(bdist = unlist(bdist), 
+#                   bd.spd.before = unlist(bd.spd.before), bd.spd.after = unlist(bd.spd.after),
+#                   edist = unlist(edist),
+#                   ed.spd.before = unlist(ed.spd.before), ed.spd.after = unlist(ed.spd.after),
+#                   disturbance = ifelse(brick %like% 'g', 'undisturbed', 'disturbed')), by = .(id, brick)]
 
-speed <- coefs[,.(bdist = unlist(bdist), 
-                  bd.spd.before = unlist(bd.spd.before), bd.spd.after = unlist(bd.spd.after),
-                  edist = unlist(edist),
-                  ed.spd.before = unlist(ed.spd.before), ed.spd.after = unlist(ed.spd.after),
-                  disturbance = ifelse(brick %like% 'g', 'undisturbed', 'disturbed')), by = .(id, brick)]
-#speed.1hr[,'brick2'] <-gsub("[^0-9.-]", "", speed.1hr$brick)
-speed[, id_treat := paste(id, brick, sep = '_')]
-speed[bd.spd.before <0, bd.spd.before:=0]
-speed[bd.spd.after <0, bd.spd.after:=0]
-speed[ed.spd.before <0, ed.spd.before:=0]
-speed[ed.spd.after <0, ed.spd.after:=0]
+# TODO: add in
+# speed[,'brick2'] <-gsub("[^0-9.-]", "", speed.1hr$brick)
+# speed[, id_treat := paste(id, brick, sep = '_')]
+# speed[bd.spd.before <0, bd.spd.before:=0]
+# speed[bd.spd.after <0, bd.spd.after:=0]
+# speed[ed.spd.before <0, ed.spd.before:=0]
+# speed[ed.spd.after <0, ed.spd.after:=0]
 
-speed.brick.before <- ggplot(data=speed[brick != 'g1' & brick != 'g3'], aes(x=bdist, y=bd.spd.before, color = brick)) + 
-  geom_line(aes(group= id_treat, linetype = brick), size=1, alpha=.5) +
-  #geom_hline(yintercept=790.9842, linetype='dashed', size = 1) +
-  geom_smooth(size = 2, se = T, method = 'glm')+
-  theme_classic() +
-  theme(text = element_text(size=15)) +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  theme(axis.text.x =  element_text(size = 15)) + 
-  #  theme(legend.position = "none") +
-  theme(plot.margin = margin(0.1, 1, .1, .1, "cm")) +
-  ggtitle("before ") +
-  xlab("Distance from brick (cm)") + ylab("Speed (cm per hour)")
+# input: speed
+plot_speed_brick <- function(DT) {
+  before <- ggplot(data=DT[brick != 'g1' & brick != 'g3'], aes(x=bdist, y=bd.spd.before, color = brick)) + 
+    geom_line(aes(group= id_treat, linetype = brick), size=1, alpha=.5) +
+    #geom_hline(yintercept=790.9842, linetype='dashed', size = 1) +
+    geom_smooth(size = 2, se = T, method = 'glm')+
+    theme_classic() +
+    theme(text = element_text(size=15)) +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    theme(axis.text.x =  element_text(size = 15)) + 
+    #  theme(legend.position = "none") +
+    theme(plot.margin = margin(0.1, 1, .1, .1, "cm")) +
+    ggtitle("before ") +
+    xlab("Distance from brick (cm)") + ylab("Speed (cm per hour)")
+  
+  after <- ggplot(data=DT[brick != 'g1' & brick != 'g3' & id != 'O24b'], aes(x=bdist, y=bd.spd.after, color = brick)) + 
+    geom_line(aes(group= id_treat, linetype = brick), size=1, alpha=.5) +
+    #geom_hline(yintercept=790.9842, linetype='dashed', size = 1) +
+    geom_smooth(size = 2, se = T, method = 'glm')+
+    theme_classic() +
+    theme(text = element_text(size=15)) +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    theme(axis.text.x =  element_text(size = 15)) + 
+    #  theme(legend.position = "none") +
+    theme(plot.margin = margin(0.1, 1, .1, .1, "cm")) +
+    ggtitle("after ") +
+    #ylim(0,1200) +
+    xlab("Distance from brick (cm)") + ylab("Speed (cm per hour)")
+  
+  before + after
+}
 
-speed.brick.before 
+plot_speed_edge <- function(DT) {
+  before <- ggplot(data=DT[brick != 'g1' & brick != 'g3' & id != 'O24b'], aes(x=edist, y=ed.spd.before, color = brick)) + 
+    geom_line(aes(group= id_treat, linetype = brick), size=1, alpha=.5) +
+    #geom_hline(yintercept=790.9842, linetype='dashed', size = 1) +
+    geom_smooth(size = 2, se = T, method = 'glm')+
+    theme_classic() +
+    theme(text = element_text(size=15)) +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    theme(axis.text.x =  element_text(size = 15)) + 
+    #  theme(legend.position = "none") +
+    theme(plot.margin = margin(0.1, 1, .1, .1, "cm")) +
+    ggtitle("before ") +
+    ylim(0,5000)+
+    xlab("Distance from edge (cm)") + ylab("Speed (cm per hour)")
+  
+  after <- ggplot(data=DT[brick != 'g1' & brick != 'g3' & id != 'O24b'], aes(x=edist, y=ed.spd.after, color = brick)) + 
+    geom_line(aes(group= id_treat, linetype = brick), size=1, alpha=.5) +
+    #geom_hline(yintercept=790.9842, linetype='dashed', size = 1) +
+    geom_smooth(size = 2, se = T, method = 'glm')+
+    theme_classic() +
+    theme(text = element_text(size=15)) +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    theme(axis.text.x =  element_text(size = 15)) + 
+    #  theme(legend.position = "none") +
+    theme(plot.margin = margin(0.1, 1, .1, .1, "cm")) +
+    ggtitle("after ") +
+    ylim(0,5000) +
+    xlab("Distance from edge (cm)") + ylab("Speed (cm per hour)")
+  
+  before + after
+}
 
-speed.brick.after <- ggplot(data=speed[brick != 'g1' & brick != 'g3' & id != 'O24b'], aes(x=bdist, y=bd.spd.after, color = brick)) + 
-  geom_line(aes(group= id_treat, linetype = brick), size=1, alpha=.5) +
-  #geom_hline(yintercept=790.9842, linetype='dashed', size = 1) +
-  geom_smooth(size = 2, se = T, method = 'glm')+
-  theme_classic() +
-  theme(text = element_text(size=15)) +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  theme(axis.text.x =  element_text(size = 15)) + 
-  #  theme(legend.position = "none") +
-  theme(plot.margin = margin(0.1, 1, .1, .1, "cm")) +
-  ggtitle("after ") +
-  #ylim(0,1200) +
-  xlab("Distance from brick (cm)") + ylab("Speed (cm per hour)")
-speed.brick.after 
-
-speed.brick.before + speed.brick.after
-
-speed.edge.before <- ggplot(data=speed[brick != 'g1' & brick != 'g3' & id != 'O24b'], aes(x=edist, y=ed.spd.before, color = brick)) + 
-  geom_line(aes(group= id_treat, linetype = brick), size=1, alpha=.5) +
-  #geom_hline(yintercept=790.9842, linetype='dashed', size = 1) +
-  geom_smooth(size = 2, se = T, method = 'glm')+
-  theme_classic() +
-  theme(text = element_text(size=15)) +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  theme(axis.text.x =  element_text(size = 15)) + 
-  #  theme(legend.position = "none") +
-  theme(plot.margin = margin(0.1, 1, .1, .1, "cm")) +
-  ggtitle("before ") +
-  ylim(0,5000)+
-  xlab("Distance from edge (cm)") + ylab("Speed (cm per hour)")
-speed.edge.before 
-
-speed.edge.after <- ggplot(data=speed[brick != 'g1' & brick != 'g3' & id != 'O24b'], aes(x=edist, y=ed.spd.after, color = brick)) + 
-  geom_line(aes(group= id_treat, linetype = brick), size=1, alpha=.5) +
-  #geom_hline(yintercept=790.9842, linetype='dashed', size = 1) +
-  geom_smooth(size = 2, se = T, method = 'glm')+
-  theme_classic() +
-  theme(text = element_text(size=15)) +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  theme(axis.text.x =  element_text(size = 15)) + 
-  #  theme(legend.position = "none") +
-  theme(plot.margin = margin(0.1, 1, .1, .1, "cm")) +
-  ggtitle("after ") +
-  ylim(0,5000) +
-  xlab("Distance from edge (cm)") + ylab("Speed (cm per hour)")
-speed.edge.after 
-
-speed.edge.before + speed.edge.after
