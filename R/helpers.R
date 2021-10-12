2ba025c32dc2c2be502b1158a989e8e26a18c8ce
Alec L. Robitaille <robit.alec@gmail.com>
2021-10-12 11:46:14 -0700

    clean helpers

--- R/helpers.R
+++ R/helpers.R
@@ -1,11 +1,31 @@
+# === Helpers -------------------------------------------------------------
+
+
+
+# Tidy coefs --------------------------------------------------------------
 tidy_coefs <- function(model, distparams) {
   if (is.null(distparams)) return(NULL)
   
   coefs <- dcast(model, level ~ term, value.var = 'estimate')
   coefs <- coefs %>% tidyr::separate((level), c('id', 'brick'), sep = '_')
   coefs <- merge(coefs, distparams, by = 'id')
+  
+  return(coefs)
 }
 
+
+
+# Tidy model --------------------------------------------------------------
+tidy_model <- function(model, effect) {
+  ran_vals <- tidy(model, effect = effect)
+  indiv.se <- setDT(ran_vals)[group == 'id_treat']
+  
+  return(indiv.se)
+}
+
+
+
+# Set factors -------------------------------------------------------------
 set_factors <- function(DT) {
   stagelevels <- c("B","A")
   ghostbricklevels <- c("g1", "g2","g3", 
@@ -13,13 +33,12 @@ set_factors <- function(DT) {
   DT[, stage := factor(stage, levels = stagelevels)]
   DT[, ghostbricks := factor(ghostbricks, levels = ghostbricklevels)]
   
+  return(DT)
 }
 
-tidy_model <- function(model, effect) {
-  ran_vals <- tidy(model, effect = effect)
-  indiv.se <- setDT(ran_vals)[group == 'id_treat']
-}
 
+
+# Clean model names -------------------------------------------------------
 # TODO: make this consistent with other tidy_model?
 clean_model_names <- function(model) {
   # making names easier to deal with
@@ -32,5 +51,5 @@ clean_model_names <- function(model) {
   model$term <- gsub('Ilogbrickdiststart1', 'brickdist_', model$term)
   model$term <- gsub('Ilogedgediststart1', 'edgedist_', model$term)
   
-  model
+  return(model)
 }
