ebbfd1a6cd428767d09c12582414af76500cabfc
Alec L. Robitaille <robit.alec@gmail.com>
2021-10-14 13:18:33 -0700

    todo summary movement

---
+++ R/summary.R
@@ -0,0 +1,9 @@
+summary_movement <- function(DT) {
+  # > DT[1:5]
+  # snail          id disturbance treatment nTotal nSteps nMove BnA
+  # 1:  O24b GeeGeronimo undisturbed         C    192    129     8   y
+  # 2:  P24b       Brian undisturbed         C    192    191     9   y
+  # 3:  O14a  DoorKeeper undisturbed         C    192    143    10   y
+  # 4:  O24a   Jellybean undisturbed         C    192    163    12   y
+  # 5:  P14a        Keme undisturbed         C    192    173    15   y
+}
