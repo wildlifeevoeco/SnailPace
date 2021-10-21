9daa1d0a1a5c826e282b2a87244e0db3435bc300
Alec L. Robitaille <robit.alec@gmail.com>
2021-10-21 11:49:19 -0700

    clean one line

--- R/models.R
+++ R/models.R
@@ -39,11 +39,8 @@ model_movement <- function(DT) {
       (1 | indiv_treat_step_id) +
       (0 + I(log(sl_ + 1)) | id_treat) +
       (0 + I(log(sl_ + 1)):stage | id_treat) +
-      (0 + I(log(
-        brickdist_start + 1
-      )):I(log(sl_ + 1)):stage | id_treat) +
-      (0 + I(log(edgedist_start + 1)):I(log(sl_ + 1)):stage |
-         id_treat),
+      (0 + I(log(brickdist_start + 1)):I(log(sl_ + 1)):stage | id_treat) +
+      (0 + I(log(edgedist_start + 1)):I(log(sl_ + 1)):stage | id_treat),
     family = poisson(),
     data = DT,
     map = list(theta = factor(c(NA, 1:10))),
