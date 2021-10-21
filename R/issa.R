d378cf8eb5f601ce6bc4a627ce7c59f2bd0be171
Alec L. Robitaille <robit.alec@gmail.com>
2021-10-21 13:54:54 -0700

    fix with dont cast

--- R/issa.R
+++ R/issa.R
@@ -42,7 +42,8 @@ make_step_id <- function(DT) {
   if (is.null(DT)) return()
   if (nrow(DT) == 0) return()
   
-  setDT(DT)[, indiv_step_id := paste(id, step_id_, sep = '_')]
+  DT$indiv_step_id <- paste(DT$id, DT$step_id_, sep = '_')
+  DT
 }
 
 # Calculate distribution parameters ---------------------------------------
